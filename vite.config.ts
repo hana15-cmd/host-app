8d1d449165e67022186f74894b07668760953475
hana15-cmd <hanaqureshi46@gmail.com>
2026-02-08 18:04:44 +0000

    pushing latest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -14,7 +13,17 @@ export default defineConfig({
         remote_app: "http://localhost:5001/assets/remoteEntry.js",
         mfe_accounting_app: "http://localhost:5002/assets/remoteEntry.js",
        },
-      shared: ["react", "react-dom", "react-router-dom"],
+      shared: {
+        react: {
+          requiredVersion: "^19.2.0",
+        },
+        "react-dom": {
+          requiredVersion: "^19.2.0",
+        },
+        "react-router-dom": {
+          requiredVersion: "^7.0.0",
+        },
+      },
     }),
   ],
   build: {
@@ -22,5 +31,15 @@ export default defineConfig({
     target: "esnext",
     minify: false,
     cssCodeSplit: false,
+  },
+  preview: {
+    port: 5003,
+    strictPort: true,
+    cors: true,
+  },
+  server: {
+    port: 5003,
+    strictPort: true,
+    cors: true,
   },
 });
