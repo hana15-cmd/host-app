25e6bffd0caac34b50a14da6634adb019d3c7a46
hana15-cmd <hanaqureshi46@gmail.com>
2026-02-26 15:17:28 +0000

    feat: implement CI/CD workflow for Host App with Docker support

--- src/mfe/remotes.ts
+++ src/mfe/remotes.ts
@@ -1,6 +1,8 @@
 import React from "react";
 
-export const DashboardApp = React.lazy(() => import("remote_app/App"));
+export const DashboardApp = React.lazy(() => 
+    import("remote_app/App"));
+
 export const AccountingApp = React.lazy(() =>
   import("mfe_accounting_app/App")
 );
